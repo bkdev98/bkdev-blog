84fd7788acdd20d8023dee3e9a833db34e950ee6
bkdev98 <bkdev98@gmail.com>
2017-04-09 20:17:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# <BKDEV /> Blog
+My weekend project using React to create a blog integrate with Wordpress RestAPI.
---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to my blog</h2>
+        </div>
+        <p className="App-intro">
+          This is first content
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
